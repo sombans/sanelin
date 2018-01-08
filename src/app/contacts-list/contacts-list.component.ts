caeb1d577debb9bfabb6364a5b14b8f9c0503f63
Academy <sanela26@gmail.com>
2018-01-08 21:19:06 +0100

    cetvrti komit

--- src/app/contacts-list/contacts-list.component.ts
+++ src/app/contacts-list/contacts-list.component.ts
@@ -6,10 +6,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contacts-list.component.css']
 })
 export class ContactsListComponent implements OnInit {
-  
-  contact='First Contact';
 
-  constructor() { }
+  //contact='First Contact';
+  contacts;
+
+  constructor() { 
+
+      this.contacts = [      {
+      firstName: 'John',
+      lastName:  'Doe',
+      email:     'john@example.com'
+    },
+    {
+      firstName: 'Daniel',
+      lastName:  'Ros',
+      email:     'daniel@example.com'
+    },
+    {
+      firstName: 'Martin',
+      lastName:  'Hess',
+      email:     'martin@example.com'
+    }
+  ];
+  }
 
   ngOnInit() {
   }
