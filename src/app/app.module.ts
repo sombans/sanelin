f930755e86df72ac95798f142ca4e65fad256013
Academy <sanela26@gmail.com>
2018-01-09 18:39:12 +0100

    deseti komit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ContactsListComponent } from './contacts-list/contacts-list.component';
 import { LeyoutComponent } from './leyout/leyout.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     ContactsListComponent,
-    LeyoutComponent
+    LeyoutComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule
